e8d8ddbd41bed76074ab776b3ca8a4413026a3c6
Richeux Antoine <richeux.antoin44@gmail.com>
2018-06-04 21:51:10 +0200

    Initial Commit

---
+++ BeApp_Bicloo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeApp_Bicloo
+//
+//  Created by RICHEUX Antoine on 04/06/2018.
+//  Copyright © 2018 Richeux Antoine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
